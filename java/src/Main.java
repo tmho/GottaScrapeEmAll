e675b16719f3aaa12533c2d0dd7794aa47071c65
Tim Osmond <tosmond@gmail.com>
2014-03-08 02:38:31 +1100

    2:30am coding after beers != efficient

--- java/src/Main.java
+++ java/src/Main.java
@@ -2,38 +2,61 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Main {
-    public static final String URL = "http://pokemondb.net/pokedex/national";
-    public static final String POKEMON_ELEMENT_CLASS = ".infocard-tall";
-    public static final String POKEMON_NAME_CLASS = "ent-name";
-    public static final String POKEMON_TYPE_CLASS = "itype";
-    public static final String OUTPUT_FILE = "pokemon.json";
-
     public static void main(String[] args) throws Exception {
-        Document doc = Jsoup.connect(URL).get();
-        Elements pokemonElements = doc.select(POKEMON_ELEMENT_CLASS);
+        Document typesDocument = Jsoup.connect("http://www.pokeffectiveness.com/").get();
         List<Pokemon> pokemonList = new ArrayList<Pokemon>();
-        for (Element element : pokemonElements) {
+
+        for (Element pokemonElement : typesDocument.select(".poke_row")) {
             Pokemon pokemon = new Pokemon();
-            pokemon.name = element.getElementsByClass(POKEMON_NAME_CLASS).html();
-            Elements types = element.getElementsByClass(POKEMON_TYPE_CLASS);
-            for (Element type : types) {
-                pokemon.types.add(type.html());
+            pokemon.name = pokemonElement.getElementsByClass("poke_name").get(0).html();
+            pokemon.pokedexNo = pokemonElement.getElementsByClass("pokedex_number").get(0).html();
+            for (Element pokemonTypeElement : pokemonElement.getElementsByClass("poke_type")) {
+                pokemon.types.add(pokemonTypeElement.html());
             }
+            addDamageMods(pokemonElement, pokemon, "superweak", 400);
+            addDamageMods(pokemonElement, pokemon, "weak", 200);
+            addDamageMods(pokemonElement, pokemon, "normal_col", 100);
+            addDamageMods(pokemonElement, pokemon, "resistant", 50);
+            addDamageMods(pokemonElement, pokemon, "superresistant", 25);
+            addDamageMods(pokemonElement, pokemon, "immune", 0);
             pokemonList.add(pokemon);
         }
+
         ObjectMapper mapper = new ObjectMapper();
-        mapper.writeValue(new File(OUTPUT_FILE), pokemonList);
+        mapper.writeValue(new File("pokemon.json"), pokemonList);
+
+    }
+
+    private static void addDamageMods(Element pokemonElements, Pokemon pokemon, String category, int modifier) {
+        for (Element pokemonElement : pokemonElements.getElementsByClass(category)) {
+            for (Element typeElement : pokemonElement.getElementsByClass("type")) {
+                pokemon.typeDamageMods.put(typeElement.html(), modifier);
+            }
+        }
+    }
+}
+
+class Type {
+    public List<String> types = new ArrayList<String>();
+    public Map<String, Integer> typeDamageMods = new HashMap<String, Integer>();
+
+    public Type(List<String> types, Map<String, Integer> typeDamageMods) {
+        this.types = types;
+        this.typeDamageMods = typeDamageMods;
     }
 }
 
 class Pokemon {
+    public String pokedexNo;
     public String name;
     public List<String> types = new ArrayList<String>();
+    public Map<String, Integer> typeDamageMods = new HashMap<String, Integer>();
 }
