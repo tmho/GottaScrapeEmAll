177f5b91eb1cc41db47fd0ff9ed52c9ff37883ce
David N. Johnson <me@dnj.io>
2014-03-27 23:44:53 +1100

    Node re-factored for readability.

--- node/pmon.js
+++ node/pmon.js
@@ -8,65 +8,68 @@ var es = require("event-stream");
 var trumpet = require("trumpet");
 var $ = require("cheerio");
 
-var map = {
-	superweak: 400,
-	weak: 200	,
-	normal_col: 100,
-	resistant: 50,
-	superresistant: 25,
-	immune: 0
+//Scrape all pokemeon from pokeffectiveness.com
+var parsePokemonList = require("stream").Transform();
+parsePokemonList._readableState.objectMode = true;
+
+parsePokemonList._transform = function (buff, enc, next) {
+	var out = this;
+	var filterRow = trumpet();
+
+	var map = {
+		superweak: 400,
+		weak: 200	,
+		normal_col: 100,
+		resistant: 50,
+		superresistant: 25,
+		immune: 0
+	};
+
+	filterRow.selectAll(".poke_row", function (row) {
+		var obj = { types: [ ], typeDamageMods: { }, stats: { } };
+
+		row.createReadStream().pipe(concat(function (html) {
+			$("span", html.toString()).each(function () {
+				if ($(this).attr("class").match(/poke_type/))
+					obj.types.push($(this).text());
+				else if (!$(this).attr("class").match(/type/))
+					obj[$(this).attr("class")] = $(this).text();
+				else
+					obj.typeDamageMods[$(this).text()] = map[$(this).parent().attr("class")];
+			});
+
+			out.push(obj);
+		}));
+	});
+
+	filterRow.write(buff);
+	next();
 };
 
-var wsStats = require("stream").Transform({ objectMode: true });
+//Scrape all stats from pokemondb.net
+var parsePokemon = require("stream").Transform({ objectMode: true });
 
-wsStats._transform = function (obj, enc, next) {
+parsePokemon._transform = function (obj, enc, next) {
 	var out = this;
-    var trStats = trumpet();
+    var filterStats = trumpet();
 
-	trStats.selectAll(".span-8 .vitals-table tbody tr", function (row) {
+	filterStats.selectAll(".span-8 .vitals-table tbody tr", function (row) {
 		row.createReadStream().pipe(concat(function (html) {
 			if (html.toString().length > 4)
 				obj.stats[$("th", html.toString()).text()] = $("td:first-child", html.toString()).eq(0).text();
 		}));
 	});
 
-	trStats.on("end", function () {
-		out.push(JSON.stringify(obj, null, 2));
+	filterStats.on("end", function () {
+		out.push(obj);
 		next();
 	});
 
-    request("http://pokemondb.net/pokedex/" + obj["poke_name"]).pipe(trStats);
+    request("http://pokemondb.net/pokedex/" + obj["poke_name"]).pipe(filterStats);
 };
 
-wsStats.on("end", function () {
-	process.stdout.write("]");
-});
-
-process.stdout.write("[");
-wsStats.pipe(es.join(",")).pipe(process.stdout);
-
-
-var trRow = trumpet();
-
-trRow.selectAll(".poke_row", function (row) {
-	var obj = { types: [ ], typeDamageMods: { }, stats: { } };
-
-	row.createReadStream().pipe(concat(function (html) {
-		$("span", html.toString()).each(function () {
-			if ($(this).attr("class").match(/poke_type/))
-				obj.types.push($(this).text());
-			else if (!$(this).attr("class").match(/type/))
-				obj[$(this).attr("class")] = $(this).text();
-			else
-				obj.typeDamageMods[$(this).text()] = map[$(this).parent().attr("class")];
-		});
-
-		wsStats.write(obj);
-	}));
-});
-
-trRow.on("end", function () {
-	wsStats.end();
-});
-
-process.stdin.pipe(trRow);
+process.stdin
+	.pipe(parsePokemonList)
+	.pipe(parsePokemon)
+	.pipe(require("JSONStream").stringify())
+	.pipe(process.stdout);
